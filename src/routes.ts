817ec96afc524a642fe23579ba61d5865d9871b2
robertomelo822 <roberto@ezzepay.com.br>
2025-03-23 18:11:49 -0300

    melhorado estrutura de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
-
 const userController = new UserController();
 
 router.get('/users', userController.getAllUsers);
